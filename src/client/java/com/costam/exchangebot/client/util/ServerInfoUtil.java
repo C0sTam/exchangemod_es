b4426b5394ddc19f7f27cb0c55ca603a3afe3e14
C0sTam <79651102+C0sTam@users.noreply.github.com>
2026-01-26 16:51:22 +0100

    feat(gui): update player slot targeting and preferred account modes for new usernames

--- src/client/java/com/costam/exchangebot/client/util/ServerInfoUtil.java
+++ src/client/java/com/costam/exchangebot/client/util/ServerInfoUtil.java
@@ -120,8 +120,8 @@ public class ServerInfoUtil {
     public static void setPreferredModeForUsername(String username) {
         if (username == null) { preferredAccountMode = null; return; }
         String u = username.trim();
-        if (u.equalsIgnoreCase("micho1") || u.equalsIgnoreCase("eswymianalf")) preferredAccountMode = "LIFESTEAL";
-        else if (u.equalsIgnoreCase("Matejson") || u.equalsIgnoreCase("eswymianabox")) preferredAccountMode = "BOXPVP";
+        if (u.equalsIgnoreCase("micho1") || u.equalsIgnoreCase("eswymianylf")) preferredAccountMode = "LIFESTEAL";
+        else if (u.equalsIgnoreCase("Matejson") || u.equalsIgnoreCase("eswymianybox")) preferredAccountMode = "BOXPVP";
         else preferredAccountMode = null;
     }
 
